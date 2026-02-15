494d3afcc16aed48796ac25cba90049fb58e915f
peabody <scottfeldmanpeabody@gmail.com>
2026-02-14 21:48:15 -0500

    collecting all race results for XC working

--- src/read_results_pdf.py
+++ src/read_results_pdf.py
@@ -2,47 +2,7 @@ import camelot
 from pathlib import Path
 import os
 import pandas as pd
-
-def import_results_pdf(local_filename, race, category):
-
-    tables = camelot.read_pdf(local_filename, pages='1', flavor='stream')
-
-    df = tables[0].df
-
-
-    format = determine_format(race)
-
-    if format == 'XC':
-        # If you know the table always starts after row 5
-        df = df.iloc[3:].reset_index(drop=True)
-
-        # Set the first row of the remaining data as the header
-        df.columns = df.iloc[0]
-        df = df.drop(df.index[0])
-        new_columns = ['Rank', 'Plate', 'Name']
-        keep_columns = df.columns[3:]
-        new_columns.extend(keep_columns)
-        df.columns = new_columns
-    elif format == 'Enduro':
-        df = df.iloc[3:].reset_index(drop=True)
-    else:
-        raise(f'Error: Undefined format: {format}')
-    df['Race'] = race
-    df['Category'] = category
-    df = df[['Race', 'Category', 'Rank', 'Plate', 'Name', 'Team', 'Time', 'Points']]
-
-    for i, row in df.iterrows():
-        if row['Plate'] == '':
-            df.drop(i, inplace=True)
-
-    cols_to_fix = ['Name', 'Team']
-
-    # Replace literal \n with a space
-    for col in cols_to_fix:
-        df[col] = df[col].astype(str).str.replace(r'\\n', ' ').str.replace('\n', ' ')
-    df.reset_index(drop=True, inplace=True)
-    print(df)
-    return df
+root_dir = Path(__file__).parent.parent
 
 
 def determine_format(race):
@@ -63,9 +23,66 @@ def determine_format(race):
 
     return format
 
+
+def import_results_pdf(local_filename, race, gender, category):
+
+    tables = camelot.read_pdf(local_filename, pages='1', flavor='stream')
+
+    df = tables[0].df
+
+
+    format = determine_format(race)
+
+    if format == 'XC':
+        # If you know the table always starts after row 5
+        df = df.iloc[3:].reset_index(drop=True)
+
+        # Set the first row of the remaining data as the header
+        df.columns = df.iloc[0]
+        df = df.drop(df.index[0])
+        new_columns = ['rank', 'plate', 'name']
+        keep_columns = df.columns[3:]
+        new_columns.extend(keep_columns)
+        df.columns = [x.lower() for x in new_columns]
+    elif format == 'Enduro':
+        df = df.iloc[3:].reset_index(drop=True)
+    else:
+        raise(f'Error: Undefined format: {format}')
+    df['race'] = race
+    df['category'] = category
+    df['gender'] = gender
+    df['full_cat'] = f'{gender}-{category}'
+    df = df[['race', 'full_cat', 'gender', 'category', 'rank', 'plate', 'name', 'team', 'time', 'points']]
+
+    for i, row in df.iterrows():
+        if row['plate'] == '':
+            df.drop(i, inplace=True)
+
+    cols_to_fix = ['name', 'team']
+
+    # Replace literal \n with a space
+    for col in cols_to_fix:
+        df[col] = df[col].astype(str).str.replace(r'\\n', ' ').str.replace('\n', ' ')
+    df.reset_index(drop=True, inplace=True)
+    return df
+
+
+def collect_all_category_results(race):
+    dir = root_dir / f'data/{race}'
+    os.chdir(dir)
+    files = os.listdir(dir)
+    df = pd.DataFrame()
+    for filename in files:
+        no_extension = filename.split('.')[0]
+        gender = no_extension.split('-')[1]
+        cat = no_extension.split('-')[2]
+        df = pd.concat([df,import_results_pdf(filename, race, gender, cat)], ignore_index=True)
+    return df
+
+
+
+
 if __name__ == "__main__":
-    file_name = Path(__file__).parent.parent
-    race = 'VTYC Ascutney - Aug 16th'
-    category = 'F-CatA'
-    filename = file_name / f'data/{race}/results-{category}.pdf'
-    df = import_results_pdf(filename, race, category)
+    race = 'VTYC Ascutney - Aug 16th'  # test XC race
+    race = 'VTYC Cochrans - Sept 13th' # test Enduro race
+    df = collect_all_category_results(race)
