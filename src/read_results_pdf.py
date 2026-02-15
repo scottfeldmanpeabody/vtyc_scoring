1e031816195b81c5f77768cbd25378f8f95c9ba5
peabody <scottfeldmanpeabody@gmail.com>
2026-02-14 23:15:03 -0500

    working on parsing enduro stages

--- src/read_results_pdf.py
+++ src/read_results_pdf.py
@@ -24,39 +24,92 @@ def determine_format(race):
     return format
 
 
+def parse_enduro_stages(df, race, category):
+    times = df.iloc[:, -1]
+    stage_names = df.columns.to_list()[-1]
+
+    stage_names = stage_names.replace(r'\\n', ' ').replace('\n', ' ')
+
+    short_race = next((item for item in ['Cochrans', 'Woodstock'] if item in race), None)
+
+    # some spacing in the stage names make it hard to parse
+    # hardcode the stage names for now
+    # TODO do this better and remove hardcode
+    stages = {'Cochrans': {'A': ['Rooster', 'Ravine', 'R and R', 'Skullys'],
+                           'B': ['Rooster', 'Ravine', 'R and R'],
+                           'C': ['Rooster', 'Ravine', 'R and R'],
+                           'Grades56': ['Rooster', 'Ravine', 'R and R']},
+              'Woodstock': {'A': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop', 'Schist Creek'],
+                            'B': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
+                            'C': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
+                            'Grades56': ['Red Line', 'Red Line 2', 'Cloud Drop']}
+              }
+
+    stage_names = stages[short_race][category]
+    stage_count = len(stage_names)
+
+    results = []
+    for i, time in enumerate(times):
+        try:
+            time = time.split(' ')
+            if len(time) < stage_count:
+                raise('Not enought times to parse')
+            result = {}
+            for j, stage in enumerate(stage_names):
+                result[stage] = time[j]
+            results.append(result)
+        except Exception as e:
+            print(f"Error with time {time}: {e}")
+            print('row:')
+            print(df.iloc[i])
+
+
+    print(results)
+
+
+
+
+    return df
+
 def import_results_pdf(local_filename, race, gender, category):
 
     tables = camelot.read_pdf(local_filename, pages='1', flavor='stream')
 
     df = tables[0].df
 
-
     format = determine_format(race)
 
-    if format == 'XC':
-        # If you know the table always starts after row 5
-        df = df.iloc[3:].reset_index(drop=True)
-
-        # Set the first row of the remaining data as the header
-        df.columns = df.iloc[0]
-        df = df.drop(df.index[0])
-        new_columns = ['rank', 'plate', 'name']
-        keep_columns = df.columns[3:]
-        new_columns.extend(keep_columns)
-        df.columns = [x.lower() for x in new_columns]
-    elif format == 'Enduro':
-        df = df.iloc[3:].reset_index(drop=True)
-    else:
+    if format not in ['XC', 'Enduro']:
         raise(f'Error: Undefined format: {format}')
+
+    df = df.iloc[3:].reset_index(drop=True)
+
+    # Set the first row of the remaining data as the header
+    df.columns = df.iloc[0]
+    df = df.drop(df.index[0])
+
+    if format == 'Enduro':
+        df, stages = parse_enduro_stages(df, race, category)
+
+    new_columns = ['rank', 'plate', 'name']
+    keep_columns = df.columns[3:]
+    new_columns.extend(keep_columns)
+    df.columns = [x.lower() for x in new_columns]
+
     df['race'] = race
     df['category'] = category
     df['gender'] = gender
     df['full_cat'] = f'{gender}-{category}'
-    df = df[['race', 'full_cat', 'gender', 'category', 'rank', 'plate', 'name', 'team', 'time', 'points']]
 
     for i, row in df.iterrows():
         if row['plate'] == '':
             df.drop(i, inplace=True)
+
+    if format == 'Enduro':
+        df.rename(columns={'name': 'team', 'plate': 'plate_name'}, inplace=True)
+        df[['plate', 'name']] = df['plate_name'].str.split(' ', n=1, expand=True)
+
+
 
     cols_to_fix = ['name', 'team']
 
@@ -64,6 +117,9 @@ def import_results_pdf(local_filename, race, gender, category):
     for col in cols_to_fix:
         df[col] = df[col].astype(str).str.replace(r'\\n', ' ').str.replace('\n', ' ')
     df.reset_index(drop=True, inplace=True)
+
+    df = df[['race', 'full_cat', 'gender', 'category', 'rank', 'plate', 'name', 'team', 'time', 'points']]
+
     return df
 
 
@@ -73,6 +129,8 @@ def collect_all_category_results(race):
     files = os.listdir(dir)
     df = pd.DataFrame()
     for filename in files:
+        if 'startlist' in filename:
+            continue
         no_extension = filename.split('.')[0]
         gender = no_extension.split('-')[1]
         cat = no_extension.split('-')[2]
