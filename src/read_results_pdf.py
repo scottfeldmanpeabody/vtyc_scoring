eb5f504a664fdce29acf6065fe654d77fbdbdaa4
peabody <scottfeldmanpeabody@gmail.com>
2026-02-15 17:37:45 -0500

    race results parsing sorta working with errors.

--- src/read_results_pdf.py
+++ src/read_results_pdf.py
@@ -35,20 +35,20 @@ def parse_enduro_stages(df, race, category):
     # some spacing in the stage names make it hard to parse
     # hardcode the stage names for now
     # TODO do this better and remove hardcode
-    stages = {'Cochrans': {'A': ['Rooster', 'Ravine', 'R and R', 'Skullys'],
-                           'B': ['Rooster', 'Ravine', 'R and R'],
-                           'C': ['Rooster', 'Ravine', 'R and R'],
+    stages = {'Cochrans': {'CatA': ['Rooster', 'Ravine', 'R and R', 'Skullys'],
+                           'CatB': ['Rooster', 'Ravine', 'R and R'],
+                           'CatC': ['Rooster', 'Ravine', 'R and R'],
                            'Grades56': ['Rooster', 'Ravine', 'R and R']},
-              'Woodstock': {'A': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop', 'Schist Creek'],
-                            'B': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
-                            'C': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
+              'Woodstock': {'CatA': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop', 'Schist Creek'],
+                            'CatB': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
+                            'CatC': ['Red Line', 'Red Line 2', 'Hard Style', 'Cloud Drop'],
                             'Grades56': ['Red Line', 'Red Line 2', 'Cloud Drop']}
               }
 
     stage_names = stages[short_race][category]
     stage_count = len(stage_names)
 
-    results = []
+    stage_results = []
     for i, time in enumerate(times):
         try:
             time = time.split(' ')
@@ -57,19 +57,16 @@ def parse_enduro_stages(df, race, category):
             result = {}
             for j, stage in enumerate(stage_names):
                 result[stage] = time[j]
-            results.append(result)
+            stage_results.append(result)
         except Exception as e:
             print(f"Error with time {time}: {e}")
             print('row:')
             print(df.iloc[i])
 
+    stage_results = pd.DataFrame(stage_results)
+    df = df.merge(stage_results, left_index=True, right_index=True, how='inner')
 
-    print(results)
-
-
-
-
-    return df
+    return df, stage_names
 
 def import_results_pdf(local_filename, race, gender, category):
 
@@ -106,19 +103,31 @@ def import_results_pdf(local_filename, race, gender, category):
             df.drop(i, inplace=True)
 
     if format == 'Enduro':
-        df.rename(columns={'name': 'team', 'plate': 'plate_name'}, inplace=True)
-        df[['plate', 'name']] = df['plate_name'].str.split(' ', n=1, expand=True)
+        try:
+            df.rename(columns={'name': 'team', 'plate': 'plate_name'}, inplace=True)
+            df[['plate', 'name']] = df['plate_name'].str.split(' ', n=1, expand=True)
+        except Exception as e:
+            print(f"Error fixing names for {race} {gender}-{category}: {e}")
 
 
-
-    cols_to_fix = ['name', 'team']
+    cols_to_fix = ['name', 'team', 'plate_name']
 
     # Replace literal \n with a space
     for col in cols_to_fix:
-        df[col] = df[col].astype(str).str.replace(r'\\n', ' ').str.replace('\n', ' ')
+        try:
+            df[col] = df[col].astype(str).str.replace(r'\\n', ' ').str.replace('\n', ' ')
+        except:
+            pass
     df.reset_index(drop=True, inplace=True)
 
-    df = df[['race', 'full_cat', 'gender', 'category', 'rank', 'plate', 'name', 'team', 'time', 'points']]
+    column_list = ['race', 'full_cat', 'gender', 'category', 'rank', 'plate', 'name', 'team']
+    if format == 'Enduro':
+        column_list.extend([x.lower() for x in stages])
+
+    try:
+        df = df[column_list]
+    except Exception as e:
+        print(f"Error reordering columns for {race} {gender}-{category}: {e}")
 
     return df
 
@@ -127,20 +136,38 @@ def collect_all_category_results(race):
     dir = root_dir / f'data/{race}'
     os.chdir(dir)
     files = os.listdir(dir)
-    df = pd.DataFrame()
+    all_cats = pd.DataFrame()
+    errors = []
     for filename in files:
-        if 'startlist' in filename:
-            continue
-        no_extension = filename.split('.')[0]
-        gender = no_extension.split('-')[1]
-        cat = no_extension.split('-')[2]
-        df = pd.concat([df,import_results_pdf(filename, race, gender, cat)], ignore_index=True)
-    return df
+        print(f'Processing {filename} ...')
+        try:
+            if 'startlist' in filename:
+                print(f'start list detected. ending.')
+                continue
+            no_extension = filename.split('.')[0]
+            gender = no_extension.split('-')[1]
+            cat = no_extension.split('-')[2]
+            this_cat = import_results_pdf(filename, race, gender, cat)
+            all_cats = pd.concat([all_cats, this_cat], ignore_index=True)
+        except Exception as e:
+            print(f"Error with filename {filename}: {e}")
+    return all_cats
 
+
+def process_all_races(race_list):
+    for race in race_list:
+        try:
+            short_race = race.split(' ')[1]
+            print(f'Processing {short_race} ...')
+            df = collect_all_category_results(race)
+            df.to_csv(root_dir / f'data/{short_race}.csv', index=False)
+            print(f'{short_race} complete.')
+        except Exception as e:
+            print(f"Error with race {race}: {e}")
 
 
 
 if __name__ == "__main__":
-    race = 'VTYC Ascutney - Aug 16th'  # test XC race
-    race = 'VTYC Cochrans - Sept 13th' # test Enduro race
-    df = collect_all_category_results(race)
+    race_list = ['VTYC Ascutney - Aug 16th', 'VTYC Cochrans - Sept 13', 'VTYC Kingdom Trails - Sept 28th',
+                 'VTYC Woodstock - Oct 12th', 'VTYC Craftsbury - Oct 19th']
+    process_all_races(race_list)
